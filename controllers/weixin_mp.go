de5710361da6cf1d9cd970400cca22b43eff5b6a
李红 <lihong@liangyibang.com>
2016-12-13 19:43:30 +0800

    update

--- controllers/weixin_mp.go
+++ controllers/weixin_mp.go
@@ -6,11 +6,7 @@ import (
 	"encoding/xml"
 	"mp-go/wx"
 	"mp-go/question"
-	"github.com/huichen/sego"
-	"mp-go/utils"
 )
-
-var segmenter sego.Segmenter
 
 type WeixinMpController struct {
 	beego.Controller
@@ -43,19 +39,6 @@ func (c *WeixinMpController) Index() {
 		beego.Error(xmlerr)
 	}
 
-
-	utils.SendSms("18910881431","hello your verfi code is 1234")
-
-
-
-	// 分词
-	text := []byte(msg.Content)
-	segments := segmenter.Segment(text)
-
-	// 处理分词结果
-	// 支持普通模式和搜索模式两种分词，见代码中SegmentsToString函数的注释。
-	beego.Info(sego.SegmentsToString(segments, false))
-
 	outMsg := question.Handler(msg)
 	if nil != outMsg {
 		obj, err := xml.Marshal(outMsg)
@@ -71,8 +54,4 @@ func (c *WeixinMpController) Index() {
 	c.Data["json"] = msg
 
 	c.ServeXML()
-}
-
-func init() {
-	segmenter.LoadDictionary("../github.com/huichen/sego/data/dictionary.txt")
 }
