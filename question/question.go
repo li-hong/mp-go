4c81066563712a365be284fbc56d59b4fd1a8473
李红 <lihong@liangyibang.com>
2016-12-13 19:48:34 +0800

    update

--- question/question.go
+++ question/question.go
@@ -7,18 +7,18 @@ import (
 
 func Handler(msg wx.WxMpXmlInMessage) interface{} {
 	textMsg := wx.TextBuilder()
-	content:=utils.GetCityWeather("北京")
+	content:=utils.GetCityWeather("昌平")
 	textMsg.Content = content
 
 	textMsg.FromUserName = msg.ToUserName
 	textMsg.ToUserName = msg.FromUserName
 
-	if msg.MsgType == wx.MSG_EVENT {
-		switch msg.Event {
-		case wx.EVENT_SUBSCRIBE:wx.UserSubscribe(msg.FromUserName)
-		case wx.EVENT_UNSUBSCRIBE:wx.UserUnSubscribe(msg.FromUserName)
-		}
-	}
+	//if msg.MsgType == wx.MSG_EVENT {
+	//	switch msg.Event {
+	//	case wx.EVENT_SUBSCRIBE:wx.UserSubscribe(msg.FromUserName)
+	//	case wx.EVENT_UNSUBSCRIBE:wx.UserUnSubscribe(msg.FromUserName)
+	//	}
+	//}
 
 	return textMsg
 }
